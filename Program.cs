6b03191c0203bcc2d4d2075ed5a9b156c22715d7
moalsayeed <mohanedalsayes@gmail.com>
2025-07-05 17:48:08 +0300

    q3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using Assignmnet01oop.NewFolder;
+using System.Security.Cryptography.X509Certificates;
 using static Assignmnet01oop.NewFolder.Class1;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Assignmnet01oop;
 
 internal class Program
@@ -19,24 +21,65 @@ internal class Program
         #endregion
 
         #region q2
-        Person[] person = new Person[3];
-         person[0] = new Person("Mohamed", 30);
-        person[1] = new Person("Bob", 25);
-        person[2] = new Person("Ahmed", 35);
-            foreach(Person p in person)
-            {
-                Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
-        }
+        //Person[] person = new Person[3];
+        // person[0] = new Person("Mohamed", 30);
+        //person[1] = new Person("Bob", 25);
+        //person[2] = new Person("Ahmed", 35);
+        //    foreach(Person p in person)
+        //    {
+        //        Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
+        //}
 
 
 
         #endregion
 
         #region q3
+        //Write a C# program that takes a season name as input from the user and displays
+        //the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+
+
+            Console.WriteLine("Enter a season (Spring, Summer, Autumn, Winter): ");
+            string input = Console.ReadLine()!;
+
+  
+        Season season;
+            bool isValidSeason = Enum.TryParse(input, true, out season);
+       
+
+       string Renge= getRenge(season);
+            if (isValidSeason)
+            {
+                Console.WriteLine($"The range for {season} is: {Renge}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid season entered.");
+        }
+
+
+
+
 
         #endregion
 
 
     }
 
+    static string getRenge(Season season)
+    {
+        switch (season)
+        {
+            case Season.Spring:
+                return "March to May";
+            case Season.Summer:
+                return "June to August";
+            case Season.Autumn:
+                return "September to November";
+            case Season.Winter:
+                return "December to February";
+            default:
+                return "Invalid season entered.";
+        }
+    }
 }
