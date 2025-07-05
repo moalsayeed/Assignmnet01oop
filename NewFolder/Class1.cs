b6e1e17b1db91144b7a929100e64ceb8252118ff
moalsayeed <mohanedalsayes@gmail.com>
2025-07-05 17:29:01 +0300

    Add project files.

---
+++ NewFolder/Class1.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Assignmnet01oop.NewFolder
+{
+    public class Class1
+    {
+        public enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Assignmnet01oop.NewFolder;
+using static Assignmnet01oop.NewFolder.Class1;
+namespace Assignmnet01oop;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+      
+        #region q1
+        //Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then,
+        //write a C# program that prints out all the days of the week using this enum.
+
+        WeekDays weekDays= new WeekDays();
+        foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+        {
+            Console.WriteLine(day);
+        }
+        #endregion
+
+        #region q2
+
+
+        #endregion
+
+
+    }
+
+}
