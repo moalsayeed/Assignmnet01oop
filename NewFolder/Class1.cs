2aa6317ce57988c43fb7d6a52e873bbc50462bb3
moalsayeed <mohanedalsayes@gmail.com>
2025-07-05 17:36:43 +0300

    q2

--- NewFolder/Class1.cs
+++ NewFolder/Class1.cs
@@ -3,6 +3,7 @@ namespace Assignmnet01oop.NewFolder
 {
     public class Class1
     {
+        // Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.
         public enum WeekDays
         {
             Monday,
@@ -13,6 +14,22 @@ namespace Assignmnet01oop.NewFolder
             Saturday,
             Sunday
         }
+
+
+        //Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then,
+        //write a C# program to display the details of all the persons in the array.
+        public struct Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,15 +11,28 @@ internal class Program
         //Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then,
         //write a C# program that prints out all the days of the week using this enum.
 
-        WeekDays weekDays= new WeekDays();
-        foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
-        {
-            Console.WriteLine(day);
-        }
+        //WeekDays weekDays= new WeekDays();
+        //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+        //{
+        //    Console.WriteLine(day);
+        //}
         #endregion
 
         #region q2
+        Person[] person = new Person[3];
+         person[0] = new Person("Mohamed", 30);
+        person[1] = new Person("Bob", 25);
+        person[2] = new Person("Ahmed", 35);
+            foreach(Person p in person)
+            {
+                Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
+        }
 
+
+
+        #endregion
+
+        #region q3
 
         #endregion
 
